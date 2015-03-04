260a860bf44fa5c9da1617b12d798f9d26dc0047
Xu Wang <xuwang@gmail.com>
2015-03-04 14:55:02 -0800

    secure dhparam.pem by chmod 0600

--- apps/certs/gen.sh
+++ apps/certs/gen.sh
@@ -9,12 +9,12 @@ openssl req -new -out site.csr -config site.cnf
 echo "signing site.csr..."
 openssl x509 -req -days 9999 -in site.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -extensions v3_req -out site.crt -extfile site.cnf
 
+#see https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+echo "generating dhparam.pem/2048 for strengthening the server security ..."
+
+openssl dhparam -out dhparam.pem 2048
 echo "keep the secret to yourself."
-chmod 600 *.key
+chmod 600 *.key *.pem
 
 echo "site.crt is generated:"
 openssl x509 -text -noout -in site.crt
-
-#see https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
-echo "generating dhparam.pem/2048 for strengthening the server security ..."
-openssl dhparam -out dhparam.pem 2048
