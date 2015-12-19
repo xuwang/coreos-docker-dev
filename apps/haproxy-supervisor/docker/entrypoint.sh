087e80b07773c67c063e44d99ca28ca5292ae8ff
Xu Wang <xuwang@gmail.com>
2015-12-18 22:17:37 -0800

    removed haproxy-iptable-wrapper.sh. added restart/reload to mange iptable block/unblock.

--- apps/haproxy-supervisor/docker/entrypoint.sh
+++ apps/haproxy-supervisor/docker/entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -e
-
 case ${1} in
-  start|reload|restart)
-    case ${1} in
-      start)
-          exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
-          ;;
-      restart)
-          # Find haproxy pid, terminate it and supervisor will restart a new one
-          pkill -x haproxy
-          ;;
-    esac
+  start)
+    exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
+    ;;
+  reload|restart)
+    # Delay SYN at the TCP level while we restart haproxy.
+    iptables -I INPUT -p tcp --match multiport --dports 80,443 --syn -j DROP
+    pgrep -x haproxy && pkill -x haproxy
+    sleep 0.5
+    iptables -D INPUT -p tcp --match multiport --dports 80,443 --syn -j DROP
+    ;;
+  status)
+    /usr/bin/supervisorctl status haproxy
     ;;
   help)
     echo "Available options:"
-    echo " start        - Starts the gitlab server (default)"
+    echo " start|reload|restart - Starts the haproxy server (default)"
+    echo " status       - Show status"
     echo " help         - Displays the help"
     echo " [command]        - Execute the specified command, eg. bash."
     ;;
